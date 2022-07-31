e3332c23ac98fc6bf93fd10ea047c109bbcd9c1b
lunz1207 <lunz1207@gmail.com>
2022-07-31 15:02:47 +0800

    chore: remove cache file

--- src/main.py
+++ src/main.py
@@ -1,31 +1,20 @@
-import os
 from io import BytesIO
 
 from minio import Minio
-from src.utils.editer import remake
-from src.model.file import File, Plan
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-from src.env import MIDDLEWARE_MINIO_SERVICE_HOST, MIDDLEWARE_MINIO_SERVICE_PORT, MIDDLEWARE_MINIO_ACCESS_KEY, MIDDLEWARE_MINIO_SECRET_KEY
+from src.env import *
 
 
 app = FastAPI(name="files")
 
-origins = [
-    # "http://localhost",
-    "*"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-cache = './cache'
-test_plans = []
 
 minioClient = Minio(
     f'{MIDDLEWARE_MINIO_SERVICE_HOST}:{MIDDLEWARE_MINIO_SERVICE_PORT}',
@@ -35,7 +24,7 @@ minioClient = Minio(
 )
 
 
-@app.post("/files/v2")
+@app.post("/files/upload")
 async def file_upload_to_minio(file: UploadFile):
     byte = BytesIO(await file.read())
     length = len(byte.getvalue())
@@ -48,93 +37,5 @@ async def file_upload_to_minio(file: UploadFile):
             "etag": result.etag
         },
         "message": "success"
-    }
-    return resp
-
-
-@app.post("/files/v2/plan")
-async def files_plan(plan: Plan):
-    test_plans.append(plan)
-    resp = {
-        "code": 200,
-        'details': test_plans,
-        "message": "success"
-    }
-    return resp
-
-
-@app.get("/files/v2/plan")
-async def get_plan():
-    resp = {
-        "code": 200,
-        'details': test_plans,
-        "message": "success"
-    }
-    return resp
-
-
-@app.post("/files/v2/download/{plan_name}")
-async def files_get_plan(plan_name):
-    for i in test_plans:
-        for k, m in i:
-            if m == plan_name:
-                for v in i.attachment:
-                    name = v['object_name']
-                    fp = f'{cache}/{name}'
-                    result = minioClient.fget_object(
-                        v['bucket_name'], name, fp)
-    resp = {
-        "code": 200,
-        'details': {},
-        "message": "success"
-    }
-    return resp
-
-
-@app.post("/files")
-async def file_upload(file: UploadFile):
-    open(f'{cache}/{file.filename}', 'wb').write(await file.read())
-    resp = {
-        "code": 200,
-        'details': {
-            "name": file.filename
-        },
-        "message": "success"
-    }
-    return resp
-
-
-@app.post("/files/remake")
-async def file_remake(file: File):
-    remake(f'{cache}/{file.name}',
-           f'{cache}/InfluxdbBackendListener-{file.name}')
-    resp = {
-        "code": 200,
-        'details': {
-        },
-        "message": "success"
-    }
-    return resp
-
-
-@app.get("/files")
-async def files_list():
-    resp = {
-        "code": 200,
-        'details': os.listdir(f'{cache}'),
-        "message": "success"
-    }
-    return resp
-
-
-@app.delete("/files/{name}")
-async def file_remove(name):
-    os.remove(f'{cache}/{name}')
-    resp = {
-
-        "code": 200,
-        'details': {},
-        "message": "success"
-
     }
     return resp
--- tests/test_files.py
+++ tests/test_files.py
@@ -2,37 +2,9 @@ import pytest
 
 
 @pytest.mark.usefixtures('init')
-class TestController():
+class TestFiles():
 
     def test_upload(self):
         with open('./README.md', 'r') as f:
-            resp = self.bs.post('/files', files={'file': f})
-        assert resp.status_code == 200
-
-    def test_upload_v2(self):
-        with open('./README.md', 'r') as f:
-            resp = self.bs.post('/files/v2', files={'file': f})
-        assert resp.status_code == 200
-
-    def test_file_list(self):
-        resp = self.bs.get('/files')
-        assert resp.status_code == 200
-
-    def test_remove(self):
-        resp = self.bs.delete('/files/README.md')
-        assert resp.status_code == 200
-
-    def test_remake(self):
-        resp = self.bs.post('/files/remake', json={'name': 'example.jmx'})
-        assert resp.status_code == 200
-
-    def test_files_plan(self):
-        resp = self.bs.post('/files/v2/plan', json={
-            'plan_name': 'str',
-            'attachment': ['1']
-        })
-        assert resp.status_code == 200
-
-    def test_files_get_plan(self):
-        resp = self.bs.post('/files/v2/download/333')
+            resp = self.bs.post('/files/upload', files={'file': f})
         assert resp.status_code == 200
