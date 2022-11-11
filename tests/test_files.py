612e548b61ee2550a0dd947aef86b3a197f31c4d
lunz1207 <lunz1207@gmail.com>
2022-11-11 21:46:13 +0800

    fix: case

--- tests/test_files.py
+++ tests/test_files.py
@@ -4,11 +4,16 @@ import pytest
 @pytest.mark.usefixtures('init')
 class TestFiles():
 
+    header = {
+        "Authorization": "admin"
+    }
+
     def test_upload(self):
         with open('./README.md', 'r') as f:
-            resp = self.bs.post('/files/upload', files={'file': f})
+            resp = self.bs.post(
+                '/files/upload', headers=self.header, files={'file': f})
         assert resp.status_code == 200
 
     def test_pull_report(self):
-        resp = self.bs.get('/files/aomaker/report/199999')
+        resp = self.bs.get('/files/aomaker/report/199999', headers=self.header)
         assert resp.status_code == 200
