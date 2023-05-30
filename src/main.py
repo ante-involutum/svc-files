75b5e218a2dc30499bea7635bc9c39c456663048
lunzhou <lunzhou@yunify.com>
2023-05-30 09:11:16 +0800

    feat: compatibility

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import shutil
+import traceback
 from io import BytesIO
 from typing import List
 from loguru import logger
@@ -8,10 +9,12 @@ from minio.error import S3Error
 
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI, Request, UploadFile, BackgroundTasks
+from fastapi import FastAPI, Request, UploadFile
 
 from src.env import *
+from src.model import Report
 from src.exceptions import FilesException
+
 
 app = FastAPI(name="files")
 
@@ -29,6 +32,31 @@ minioClient = Minio(
     secret_key=MINIO_SECRET_KEY,
     secure=False
 )
+
+
+@app.on_event("startup")
+async def startup_event():
+    try:
+        objects = list(minioClient.list_objects(
+            'templates',
+            prefix='404',
+            recursive=True
+        ))
+        if len(objects) != 2:
+            logger.info('404 html not exist in tempplates bucket')
+            minioClient.fput_object(
+                'templates',
+                '404/index.html',
+                '404/index.html'
+            )
+            minioClient.fput_object(
+                'templates',
+                '404/styles.css',
+                '404/styles.css'
+            )
+            logger.info('push 404 html to tempplates bucket')
+    except Exception as e:
+        logger.error(traceback.format_exc())
 
 
 @app.exception_handler(FilesException)
@@ -84,7 +112,7 @@ async def get_object(bucket_name: str, prefix: str):
 
 
 @app.post("/files/upload/")
-async def file_upload_to_minio(bucket_name: str, files: List[UploadFile]):
+async def upload(bucket_name: str, files: List[UploadFile]):
 
     details = []
 
@@ -121,8 +149,82 @@ async def get_report(bucket_name: str, type: str, prefix: str):
     elif type == 'hatbox':
         return {"url": f"http://{HOST}:{PORT}/share/{prefix}/hatbox/Log/report/pytest_html"}
 
-@app.get("/files/generate_report/{bucket_name}/{prefix}")
-async def pull_form_minio(bucket_name: str, prefix: str, background_tasks: BackgroundTasks):
-    background_tasks.add_task(pull, bucket_name, prefix)
-    logger.info(f'Generate_report {prefix} in the background')
-    return {"message": "Generate report in the background"}
+
+@app.get("/files/v1.1")
+async def get_object_v1(bucket_name: str, prefix: str):
+    try:
+        data = minioClient.get_object(bucket_name, prefix)
+        detail = {'code': 0, 'detail': data.data, 'message': 'success'}
+        return detail
+    except S3Error as e:
+        logger.debug(e)
+        raise FilesException(
+            code=-1,
+            detail={},
+            message=e.message
+        )
+
+
+@app.post("/files/v1.1")
+async def upload_to_minio(bucket_name: str, files: List[UploadFile]):
+    try:
+        details = []
+        for file in files:
+            byte = BytesIO(await file.read())
+            length = len(byte.getvalue())
+            result = minioClient.put_object(
+                bucket_name, file.filename, byte, length
+            )
+            details.append({
+                'bucket_name': result.bucket_name,
+                'object_name': result.object_name,
+                "etag": result.etag
+            })
+        resp = {
+            "code": 0,
+            'details': details,
+            "message": "success"
+        }
+        logger.info(resp)
+        return resp
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        raise FilesException(code=-1, detail={}, message='内部错误')
+
+
+@app.get("/files/v1.1/report")
+async def get_report_v1(report: Report):
+    prefix = f'{report.type}-{report.uid}'
+    if os.path.exists(f'share/{prefix}'):
+        shutil.rmtree(f'share/{prefix}', ignore_errors=True)
+    else:
+        pass
+
+    try:
+        objects = list(minioClient.list_objects(
+            'result',
+            prefix=prefix,
+            recursive=True
+        ))
+        if len(objects) == 0:
+            minioClient.fget_object(
+                'templates',
+                '404/index.html',
+                f'share/{prefix}/{report.path}/index.html'
+            )
+            minioClient.fget_object(
+                'templates',
+                '404/styles.css',
+                f'share/{prefix}/{report.path}/styles.css'
+            )
+        else:
+            for obj in objects:
+                minioClient.fget_object(
+                    'result',
+                    obj.object_name,
+                    f'share/{obj.object_name}'
+                )
+        return {"url": f"http://{HOST}:{PORT}/share/{prefix}{report.path}"}
+    except Exception as e:
+        logger.error(traceback.format_exc())
+        raise FilesException(code=-1, detail={}, message='内部错误')
--- tests/test_files.py
+++ tests/test_files.py
@@ -18,24 +18,49 @@ class TestFiles():
             })
         assert resp.status_code == 200
 
-    def test_generate_report(self):
-        resp = self.bs.get(
-            '/files/generate_report/result/db95fd',
-            headers=self.header
-        )
-        assert resp.status_code == 200
-
     def test_get_report(self):
         resp = self.bs.get(
-            '/files/report/result/aomaker/lunz864bd169-4647-44cc-a20d-b5fbdfcee3e7', headers=self.header)
+            '/files/report/result/aomaker/lunz-091143e5-464e-4704-8438-04ecc98f4b1a', headers=self.header)
         assert resp.status_code == 200
 
     def test_get_object(self):
         resp = self.bs.get(
             '/files/',
             params={
-                "prefix": "df9c4d/demo/report/test.jtl",
+                "prefix": "lunz-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
                 'bucket_name': 'result'
             },
+            headers=self.header)
+        assert resp.status_code == 200
+
+    def test_post_object(self):
+        resp = self.bs.post(
+            '/files/v1.1',
+            headers=self.header,
+            files={'files': open('./README.md', 'r')},
+            params={
+                'bucket_name': 'test'
+            })
+        assert resp.status_code == 200
+
+    def test_get_object_v1(self):
+        resp = self.bs.get(
+            '/files/v1.1',
+            params={
+                "prefix": "lunz-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
+                'bucket_name': 'result'
+            },
+            headers=self.header)
+        assert resp.status_code == 200
+
+    def test_get_report_v1(self):
+        payload = {
+            "uid": '091143e5-464e-4704-8438-04ecc98f4b1a',
+            "type": "hatbox",
+            'path': '/hatbox/Log/report/pytest_html'
+        }
+        resp = self.bs.get(
+            '/files/v1.1/report',
+            json=payload,
             headers=self.header)
         assert resp.status_code == 200
