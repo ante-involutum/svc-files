fad4160be6b9d7cd7b1bbe14a386ae1b3c1f2b1c
lunzhou <lunzhou@yunify.com>
2023-07-05 08:55:26 +0800

    chore: cases

--- tests/test_files.py
+++ tests/test_files.py
@@ -20,14 +20,18 @@ class TestFiles():
 
     def test_get_report(self):
         resp = self.bs.get(
-            '/files/report/result/aomaker/lunz-091143e5-464e-4704-8438-04ecc98f4b1a', headers=self.header)
+            '/files/report/result/aomaker/qingcloud-autotest-83-1052', headers=self.header)
+        assert resp.status_code == 200
+
+    def test_tasks(self):
+        resp = self.bs.get('/files/tasks', headers=self.header)
         assert resp.status_code == 200
 
     def test_get_object(self):
         resp = self.bs.get(
             '/files/',
             params={
-                "prefix": "lunz-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
+                "prefix": "qingcloud-autotest-83-1052/data/autotest/reports/html/widgets/summary.json",
                 'bucket_name': 'result'
             },
             headers=self.header)
@@ -47,7 +51,7 @@ class TestFiles():
         resp = self.bs.get(
             '/files/v1.1',
             params={
-                "prefix": "lunz-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
+                "prefix": "qingcloud-autotest-83-1052/data/autotest/reports/html/widgets/summary.json",
                 'bucket_name': 'result'
             },
             headers=self.header)
