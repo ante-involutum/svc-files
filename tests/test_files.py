20ae01d96702c0b44fbb01f120c14ba45e401965
lunzhou <lunzhou@yunify.com>
2023-07-11 17:33:09 +0800

    feat: hatbox report and logs

--- tests/test_files.py
+++ tests/test_files.py
@@ -4,14 +4,9 @@ import pytest
 @pytest.mark.usefixtures('init')
 class TestFiles():
 
-    header = {
-        "Authorization": "admin"
-    }
-
     def test_upload(self):
         resp = self.bs.post(
-            '/files/upload/',
-            headers=self.header,
+            f'{self.url}/files/upload/',
             files={'files': open('./README.md', 'r')},
             params={
                 'bucket_name': 'test'
@@ -20,27 +15,26 @@ class TestFiles():
 
     def test_get_report(self):
         resp = self.bs.get(
-            '/files/report/result/aomaker/qingcloud-autotest-83-1052', headers=self.header)
+            f'{self.url}/files/report/result/hatbox/hatbox-091143e5-464e-4704-8438-04ecc98f4b1a')
         assert resp.status_code == 200
 
     def test_tasks(self):
-        resp = self.bs.get('/files/tasks', headers=self.header)
+        resp = self.bs.get(f'{self.url}/files/tasks')
         assert resp.status_code == 200
 
     def test_get_object(self):
         resp = self.bs.get(
-            '/files/',
+            f'{self.url}/files/',
             params={
-                "prefix": "qingcloud-autotest-83-1052/data/autotest/reports/html/widgets/summary.json",
+                "prefix": "hatbox-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
                 'bucket_name': 'result'
-            },
-            headers=self.header)
+            }
+        )
         assert resp.status_code == 200
 
     def test_post_object(self):
         resp = self.bs.post(
-            '/files/v1.1',
-            headers=self.header,
+            f'{self.url}/files/v1.1',
             files={'files': open('./README.md', 'r')},
             params={
                 'bucket_name': 'test'
@@ -49,12 +43,12 @@ class TestFiles():
 
     def test_get_object_v1(self):
         resp = self.bs.get(
-            '/files/v1.1',
+            f'{self.url}/files/v1.1',
             params={
-                "prefix": "qingcloud-autotest-83-1052/data/autotest/reports/html/widgets/summary.json",
+                "prefix": "hatbox-091143e5-464e-4704-8438-04ecc98f4b1a/data/autotest/reports/html/widgets/summary.json",
                 'bucket_name': 'result'
-            },
-            headers=self.header)
+            }
+        )
         assert resp.status_code == 200
 
     def test_get_report_v1(self):
@@ -64,7 +58,7 @@ class TestFiles():
             'path': '/hatbox/Log/report/pytest_html'
         }
         resp = self.bs.get(
-            '/files/v1.1/report',
+            f'{self.url}/files/v1.1/report',
             json=payload,
-            headers=self.header)
+        )
         assert resp.status_code == 200
